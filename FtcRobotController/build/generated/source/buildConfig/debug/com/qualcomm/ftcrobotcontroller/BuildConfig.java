d95df4e888f3d35c89d2d81465846340e59deb7d
TomP142 <tomasekpokorny02@gmail.com>
2022-10-11 16:22:37 -0500

    idk two dont use this please

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,9 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
+<<<<<<< HEAD
   public static final String APP_BUILD_TIME = "2022-10-05T18:02:45.654-0500";
+=======
+  public static final String APP_BUILD_TIME = "2022-10-03T20:19:37.031-0500";
+>>>>>>> parent of f24fc33 (.)
 }
