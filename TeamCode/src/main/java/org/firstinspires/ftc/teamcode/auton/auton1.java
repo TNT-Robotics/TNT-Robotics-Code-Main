6a5abc59e83c7a8ddf212fff0cbfaed35cd73a8d
TomP142 <tomasekpokorny02@gmail.com>
2022-12-06 16:59:58 -0600

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/auton1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/auton1.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.auton;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.misc.config;
 
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 
+@Autonomous
 public class auton1 extends LinearOpMode {
 
     config cfg = new config();
